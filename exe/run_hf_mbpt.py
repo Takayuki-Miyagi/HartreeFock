29b485b5a353900a273074734161bc6dda17fdd0
Takayuki Miyagi <m1yag1.takayuk1@gmail.com>
2019-07-30 11:34:22 -0700

    script tweaked

--- exe/run_hf_mbpt.py
+++ exe/run_hf_mbpt.py
@@ -55,15 +55,15 @@ def gen_script(params, file_nn, file_3n, batch, machine):
         prt += "#PBS -l mem=128gb,nodes=1:ppn=32,walltime=288:00:00 \n"
         prt += "cd $PBS_O_WORKDIR\n"
     if(machine=="cedar"):
-        header = "#!/bin/bash\n"
-        header += "#SBATCH --account="+account+"\n"
-        header += "#SBATCH --nodes=1\n"
-        header += "#SBATCH --ntasks=1\n"
-        header += "#SBATCH --cpus-per-task=1\n"
-        header += "#SBATCH --mem=125G\n"
-        header += "#SBATCH --time=3-00:00\n\n"
+        prt = "#!/bin/bash\n"
+        prt += "#SBATCH --account="+account+"\n"
+        prt += "#SBATCH --nodes=1\n"
+        prt += "#SBATCH --ntasks=1\n"
+        prt += "#SBATCH --cpus-per-task=32\n"
+        prt += "#SBATCH --mem=125G\n"
+        prt += "#SBATCH --time=0-03:00\n\n"
 
-    prt += 'echo "run ' +fsh + '..."\n'
+    prt += 'echo "start ' +fsh + '..."\n'
     prt += "cat > "+file_input + " <<EOF\n"
     prt += "&input\n"
     for key, value in params.items():
@@ -133,7 +133,7 @@ def main(machinename=None):
                     if(machine=="oak"):
                         cmd = "qsub " + fsh
                     if(machine=="cedar"):
-                        cmd = "srun " + fsh
+                        cmd = "sbatch " + fsh
                     subprocess.call(cmd,shell=True)
 
 if(__name__ == '__main__'):
