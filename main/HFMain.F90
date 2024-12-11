18ba00b0a8a89fe2edf6d8f020b7514b7b30c3f4
miyagi <miyagi@miyagis-Laptop.local>
2024-12-11 16:45:09 +0900

    Added HF basis things.

--- main/HFMain.F90
+++ main/HFMain.F90
@@ -167,8 +167,11 @@ program HFMain
             & PT%e_0+PT%e_2+PT%e_3
 
         if(p%is_Op_out) then ! This can be an input of an NCSM calculation
-          call w%SetFileName(p%OpFileName, htr)
-          call w%writef(p,htr)
+          call htr%UnNormalOrdering2B()
+          call msNAT%init(Nucl=ms%Nucl, Core=ms%Core, hw=ms%hw, emax=p%emax_mbpt, e2max=p%e2max_mbpt, e3max=0)
+          h = htr%truncate(msNAT)
+          call w%SetFileName(p%OpFileName, h)
+          call w%writef(p,h)
         end if
       end if
     end if
